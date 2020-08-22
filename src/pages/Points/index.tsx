0025a5aebb5e4eb77c4ec3141c4854b048e99f03
Daniel <daniel.n10@hotmail.com>
2020-08-22 18:44:58 -0300

    buscando os itens na API

--- src/pages/Points/index.tsx
+++ src/pages/Points/index.tsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Constants from 'expo-constants'
 import { Feather as Icon } from '@expo/vector-icons'
 import { useNavigation, NavigationContainer } from '@react-navigation/native'
 import { Text, View, StyleSheet, TouchableOpacity, ScrollView, Image } from 'react-native'
 import MapView, { Marker } from 'react-native-maps'
 import { SvgUri } from 'react-native-svg'
+import api from '../../services/api'
+
+interface Item {
+  id: number;
+  title: string;
+  image_url: string;
+}
 
 const Points = () => {
+    const [items, setItems] = useState<Item[]>([])
+    const [selectedItems, setSelectedItems] = useState<number[]>([])
+
     const navigation = useNavigation()
+
+    useEffect(() => {
+      api.get('items').then(response => {
+        setItems(response.data)
+      })      
+    }, [])
 
     function handleNavigateBack() {
         navigation.goBack()
@@ -16,6 +32,20 @@ const Points = () => {
     function handleNavigateToDetail() {
       navigation.navigate('Detail')
     }
+
+    function handleSelectItem(id: number) {
+      const alreadySelected = selectedItems.findIndex(item => item === id)
+
+      if (alreadySelected >= 0) {
+          const filteredItems = selectedItems.filter(item => item !== id)
+
+          setSelectedItems(filteredItems)
+      } else {
+          setSelectedItems([...selectedItems, id ])
+
+      }
+
+  }
     
     return (
       <>
@@ -53,36 +83,21 @@ const Points = () => {
           <ScrollView 
             horizontal      showsHorizontalScrollIndicator={false}
             contentContainerStyle={{paddingHorizontal: 20}}
-          > 
-            <TouchableOpacity style={styles.item} onPress={() => {}}>
-            <SvgUri width={42} height={42} uri="http://192.168.0.109:3333/uploads/lampadas.svg" />
-            <Text style={styles.itemTitle}>Lâmpadas</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.item} onPress={() => {}}>
-            <SvgUri width={42} height={42} uri="http://192.168.0.109:3333/uploads/lampadas.svg" />
-            <Text style={styles.itemTitle}>Lâmpadas</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.item} onPress={() => {}}>
-            <SvgUri width={42} height={42} uri="http://192.168.0.109:3333/uploads/lampadas.svg" />
-            <Text style={styles.itemTitle}>Lâmpadas</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.item} onPress={() => {}}>
-            <SvgUri width={42} height={42} uri="http://192.168.0.109:3333/uploads/lampadas.svg" />
-            <Text style={styles.itemTitle}>Lâmpadas</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.item} onPress={() => {}}>
-            <SvgUri width={42} height={42} uri="http://192.168.0.109:3333/uploads/lampadas.svg" />
-            <Text style={styles.itemTitle}>Lâmpadas</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.item} onPress={() => {}}>
-            <SvgUri width={42} height={42} uri="http://192.168.0.109:3333/uploads/lampadas.svg" />
-            <Text style={styles.itemTitle}>Lâmpadas</Text>
-            </TouchableOpacity>
+          >
+            {items.map(item => (
+              <TouchableOpacity 
+              key={String(item.id)} 
+              style={[
+                styles.item,
+                selectedItems.includes(item.id) ? styles.selectedItem : {}
+              ]} 
+              onPress={() => handleSelectItem(item.id)}
+              activeOpacity={0.6}
+              >
+              <SvgUri width={42} height={42} uri={item.image_url} />
+            <Text style={styles.itemTitle}>{item.title}</Text>
+              </TouchableOpacity>
+            ))}
           </ScrollView>
         </View>
       </>
