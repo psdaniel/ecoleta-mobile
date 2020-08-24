843d2106e8a2adf20cd084162ea58b55b0977743
Daniel <daniel.n10@hotmail.com>
2020-08-24 12:02:25 -0300

    configuração da posição inicial do mapa

--- src/pages/Points/index.tsx
+++ src/pages/Points/index.tsx
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from 'react'
 import Constants from 'expo-constants'
 import { Feather as Icon } from '@expo/vector-icons'
 import { useNavigation, NavigationContainer } from '@react-navigation/native'
-import { Text, View, StyleSheet, TouchableOpacity, ScrollView, Image } from 'react-native'
+import { Text, View, StyleSheet, TouchableOpacity, ScrollView, Image, Alert } from 'react-native'
 import MapView, { Marker } from 'react-native-maps'
 import { SvgUri } from 'react-native-svg'
+import * as Location from 'expo-location'
 import api from '../../services/api'
 
 interface Item {
@@ -17,13 +18,39 @@ const Points = () => {
     const [items, setItems] = useState<Item[]>([])
     const [selectedItems, setSelectedItems] = useState<number[]>([])
 
+    const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0])
+
     const navigation = useNavigation()
+
+    useEffect(() => {
+      async function loadPosition() {
+        const { status } = await Location.requestPermissionsAsync()
+
+        if(status !== 'granted') {
+          Alert.alert('Ooooops..', 'Precisamos de sua permissão para obter a localização')
+          return
+        }
+        const location = await Location.getCurrentPositionAsync()
+
+        const { latitude, longitude } = location.coords
+
+        setInitialPosition([
+          latitude,
+          longitude
+        ])
+
+      }
+
+      loadPosition()
+    }, [])
 
     useEffect(() => {
       api.get('items').then(response => {
         setItems(response.data)
       })      
     }, [])
+
+    useEffe
 
     function handleNavigateBack() {
         navigation.goBack()
@@ -56,27 +83,29 @@ const Points = () => {
           <Text style={styles.title}>Bem vindo.</Text>
           <Text style={styles.description}>Encontre no mapa um ponto de coleta.</Text>
           <View style={styles.mapContainer}>
-            <MapView 
-            style={styles.map} 
-            initialRegion={{
-              latitude: -22.9758144,
-              longitude: -43.339121,
-              latitudeDelta: 0.014,
-              longitudeDelta: 0.014,
-              }}> 
-              <Marker 
-              style={styles.mapMarker}
-              onPress={handleNavigateToDetail}
-              coordinate={{
-              latitude: -22.9758144,
-              longitude: -43.339121,
-              }}>
-                <View style={styles.mapMarkerContainer}>
-                <Image style={styles.mapMarkerImage} source={{ uri:'https://images.unsplash.com/photo-1578916171728-46686eac8d58?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80'}}/>
-                <Text style={styles.mapMarkerTitle}>Mercado</Text>
-              </View>
-              </Marker>
-            </MapView>
+            { initialPosition[0] !== 0 && (
+              <MapView 
+              style={styles.map}
+              initialRegion={{
+                latitude: initialPosition[0],
+                longitude: initialPosition[1],
+                latitudeDelta: 0.014,
+                longitudeDelta: 0.014,
+                }}> 
+                <Marker 
+                style={styles.mapMarker}
+                onPress={handleNavigateToDetail}
+                coordinate={{
+                latitude: -22.9758144,
+                longitude: -43.339121,
+                }}>
+                  <View style={styles.mapMarkerContainer}>
+                  <Image style={styles.mapMarkerImage} source={{ uri:'https://images.unsplash.com/photo-1578916171728-46686eac8d58?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80'}}/>
+                  <Text style={styles.mapMarkerTitle}>Mercado</Text>
+                </View>
+                </Marker>
+              </MapView>
+            ) }
           </View>
         </View>
         <View style={styles.itemsContainer}>
